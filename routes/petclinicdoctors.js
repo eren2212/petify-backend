825a72c639fbd199e51a723079019d04087c4f62
unknown <eiridere@gmail.com>
2025-12-15 23:03:25 +0300

    Doktorlar için sayfa hazırlığı yapıldı

---
+++ routes/petclinicdoctors.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const supabase = require("../database/supabase");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
+const { verifyToken } = require("../middleware/auth");
+const router = express.Router();
